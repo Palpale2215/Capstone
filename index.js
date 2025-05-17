e43a3b8ea500468d5846abf9090f7c6509994458
Arifal Aghna Firdian <aaghnafirdianarifal@gmail.com>
2025-05-17 20:34:53 +0700

    fix

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const AWS = require("aws-sdk");
-const cors = require("cors");
-
-AWS.config.update({ region: "us-east-1" }); // Ganti region
-
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+
+AWS.config.update({
+  region: process.env.AWS_REGION,
+});
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 app.get("/data", async (req, res) => {
-  const params = {
-    TableName: "SensorData",
-    Limit: 10,
-    ScanIndexForward: false,
-  };
   try {
-    const data = await dynamoDB.scan(params).promise();
-    res.json(data.Items);
+    const result = await dynamoDB.scan({ TableName: "SensorData" }).promise();
+    res.json(result.Items);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log("Server is running on port 3000");
 });
