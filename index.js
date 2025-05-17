2c9dbbe841ed8049a430d651dd0bcb8889b7cee3
Arifal Aghna Firdian <aaghnafirdianarifal@gmail.com>
2025-05-17 22:39:53 +0700

    fix

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const AWS = require("aws-sdk");
 
 const app = express();
 app.use(express.json());
 
+// Serve static files dari folder "public"
+app.use(express.static(path.join(__dirname, "public")));
 // Konfigurasi AWS SDK dengan env variables, termasuk session token
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -25,6 +28,10 @@ app.get("/data", async (req, res) => {
     console.error("Error baca data dari DynamoDB:", err);
     res.status(500).json({ error: "Gagal ambil data", detail: err.message });
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
